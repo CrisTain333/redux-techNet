ec715a88bf7a502fb1e5fb7874438e6ccb636fab
Sukanta Das <arnobh84@gmail.com>
2023-07-07 10:50:56 +0600

    add to cart function added

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -13,11 +13,13 @@ import {
 } from 'react-icons/hi';
 import { Button } from './ui/button';
 import { IProduct } from '@/types/globalTypes';
+import { useAppSelector } from '@/redux/hooks/hooks';
 
 export default function Cart() {
   //! Dummy data
+  const { products } = useAppSelector((state) => state.cart);
 
-  const products: IProduct[] = [];
+  // const products: IProduct[] = [];
   const total = 0;
 
   //! **
