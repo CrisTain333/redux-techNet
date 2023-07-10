973ae7cec9147853ac5eb4fe7b52d65e0d401afa
Sukanta Das <arnobh84@gmail.com>
2023-07-10 11:13:13 +0600

    code splited useing injection

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -4,32 +4,5 @@ export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000' }),
   tagTypes: ['comment'],
-  endpoints: (builder) => ({
-    getProducts: builder.query({
-      query: () => '/products',
-    }),
-    getSingleProducts: builder.query({
-      query: (productId) => `/product/${productId}`,
-    }),
-
-    postComment: builder.mutation({
-      query: ({ id, data }) => ({
-        url: `/comment/${id}`,
-        method: `POST`,
-        body: data,
-      }),
-      invalidatesTags: ['comment'],
-    }),
-    getComments: builder.query({
-      query: (id) => `/comment/${id}`,
-      providesTags: ['comment'],
-    }),
-  }),
+  endpoints: () => ({}),
 });
-
-export const {
-  useGetProductsQuery,
-  useGetSingleProductsQuery,
-  usePostCommentMutation,
-  useGetCommentsQuery,
-} = api;
